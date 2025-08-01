1f17557ed54b83a2f1f49236df36b118a1f3d92f
Yelisey <elickpro228@gmail.com>
2025-08-01 15:34:27 +0300

    add userRepository

---
+++ internal/user/repository.go
@@ -0,0 +1,30 @@
+package user
+
+import "go-advance/pkg/db"
+
+type UserRepository struct {
+	*db.Db
+}
+
+func (r *UserRepository) NewUserRepository(db *db.Db) *UserRepository {
+	return &UserRepository{db}
+}
+
+func (r *UserRepository) Create(user *User) error {
+	result := r.DB.Create(user)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func (r *UserRepository) FindByEmail(email string) (*User, error) {
+	var user *User
+	result := r.DB.First(user, "email = ?", email)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return user, nil
+}
